f51500974a2e5797cbc2c5ffde1a98928ceb0d79
asd0 <asddd08326@gmail.com>
2026-06-16 17:55:38 -0500

    login-cliente component, auth-login-request and auth-login-service.ts

---
+++ src/app/services/auth-login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthLoginRequest } from '../model/api/request/auth-login-request';
+import { inject } from '@angular/core/primitives/di';
+import { ClienteResponse } from '../model/api/response/cliente-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthLoginService {
+  private api = 'http://localhost:8080/api/v1/cliente';
+  private http = inject(HttpClient);
+  login(credentials: AuthLoginRequest): Observable<ClienteResponse> {
+    return this.http.post<ClienteResponse>(this.api, credentials);
+  }
+}
